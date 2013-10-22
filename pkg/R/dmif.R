3cd0f441e60e85f1ab82a4557a7668d96801d650
cpardy (Chris Pardy) <cpardy@unsw.edu.au>
2013-10-22 01:39:12 +0000

    Add GPL3 headers to all code
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mpmi@51 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/dmif.R
+++ pkg/R/dmif.R
@@ -1,3 +1,22 @@
+#  
+#     Copyright 2013 Chris Pardy <cpardy@unsw.edu.au>
+# 
+#     This file is part of the mpmi R package.
+# 
+#     This program is free software: you can redistribute it and/or modify
+#     it under the terms of the GNU General Public License as published by
+#     the Free Software Foundation, version 3.
+# 
+#     This program is distributed in the hope that it will be useful,
+#     but WITHOUT ANY WARRANTY; without even the implied warranty of
+#     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#     GNU General Public License for more details.
+# 
+#     You should have received a copy of the GNU General Public License
+#     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#  
+
+
 dmi.pw <- function(disc1, disc2)
 {
     disc1 <- as.integer(factor(disc1))
--- pkg/src/nawrap.c
+++ pkg/src/nawrap.c
@@ -1,8 +1,27 @@
+ /*
+     Copyright 2013 Chris Pardy <cpardy@unsw.edu.au>
+ 
+     This file is part of the mpmi R package.
+ 
+     This program is free software: you can redistribute it and/or modify
+     it under the terms of the GNU General Public License as published by
+     the Free Software Foundation, version 3.
+ 
+     This program is distributed in the hope that it will be useful,
+     but WITHOUT ANY WARRANTY; without even the implied warranty of
+     MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+     GNU General Public License for more details.
+ 
+     You should have received a copy of the GNU General Public License
+     along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+
 #include <R.h>
 
-// Value of missing integers (INT_MIN)
+/* Value of missing integers (INT_MIN) */
 int F77_SUB(rnaint)(void) { return(R_NaInt); }
 
-// Function to check for missing real values
+/* Function to check for missing real values */
 int F77_SUB(rfinite)(double *x) { return(R_FINITE(*x)); }
 
